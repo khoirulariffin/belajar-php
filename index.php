31cd28c0309a4750013475607da597c02ab46074
khoirulariffin <khoirulariffin@gmail.com>
2021-02-23 00:43:52 +0700

    tipe data

--- index.php
+++ index.php
@@ -16,4 +16,36 @@
     $f = NULL; //null
 
     echo var_dump($e);
+
+
+    //operator
+
+    //aritmatika
+    $a = 10;
+    $b = 20;
+    $total = $a-$b;
+    echo($total);
+
+    //assigment
+    $total1 = $total + $a;
+    $total += $a;
+
+    //pembanding
+    $komparasi = $a == $b; //cek isi saja
+    $komparasi = $a === $b; //cek isi & tipe data
+    
+    $komparasi = $a != $b; //cek isi saja
+    $komparasi = $a !== $b; //cek isi & tipe data
+
+    $komparasi = $a < $b; //lebih kecil
+    $komparasi = $a > $b; //lebih besar
+
+    //increment
+    $a = $a + 1;
+    $a++;
+
+    //String
+    $a = "hello";
+    $b = "world";
+    echo $a . $b;
 ?>
