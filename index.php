10e69ca78dfe35d665f514168f576bec64c7812e
khoirulariffin <khoirulariffin@gmail.com>
2021-02-23 00:15:42 +0700

    tipe data

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
-    echo("hai"); //ini untuk menampilkan output
+    echo("hai");
+    echo("<br>"); //ini untuk menampilkan output
 
     //variabel
-    $teks = "ini adalah variabel";
-    $angka = 1;
+    $teks = "ini adalah contoh variable";
+
+    //tipe data
+    $a = "ini adalah String"; //string
+    $b = 10; //integer
+    $c = 12.5; //float
+    $d = false; //boolean
+    $e = [
+        'satu', 'dua', 'tiga'
+    ]; //array
+    $f = NULL; //null
+
+    echo var_dump($e);
 ?>
